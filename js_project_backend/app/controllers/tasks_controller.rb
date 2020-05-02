94e1938093478808252b4fb6ed21648763a7203b
helloimiskra <helloimiskra@gmail.com>
2020-05-02 01:47:15 -0400

    adds tasks controller actions

--- js_project_backend/app/controllers/tasks_controller.rb
+++ js_project_backend/app/controllers/tasks_controller.rb
@@ -1,2 +1,36 @@
 class TasksController < ApplicationController
+    
+    def index
+        users = Task.all 
+        render json: tasks 
+    end
+
+    def show
+        task = Task.find_by(id: params[:id])
+        if task
+            render json: task
+        else
+            render json: {message: 'Task not found'}
+        end
+    end
+
+    def create
+        if Task.find_by(title: task_params[:title])
+            task = Task.find_by(title: task_params[:title])
+            redirect_to "/tasks/#{task.id}"
+        else
+            task = Task.create(task_params)
+            render json: task
+        end
+    end
+
+    def edit
+        task = Task.find(params[:id])
+    end
+
+    def update
+        task = Task.find(params[:id])
+        task.update(complete: params[:complete])
+        redirect_to task_path(task)
+    end
 end
