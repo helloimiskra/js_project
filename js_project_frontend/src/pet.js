6ba22542fa51642144bc3653c74f2626cd246ed4
helloimiskra <helloimiskra@gmail.com>
2020-12-09 22:40:38 +0900

    formats code

--- js_project_frontend/src/pet.js
+++ js_project_frontend/src/pet.js
@@ -1,46 +1,44 @@
 class Pet {
-    constructor(pet){
-        this.id = pet.id
-        this.name = pet.name
-        this.kind = pet.kind
-        this.user_id = pet.user_id
-        this.tasks = pet.tasks
+    constructor(pet) {
+        this.id = pet.id;
+        this.name = pet.name;
+        this.kind = pet.kind;
+        this.user_id = pet.user_id;
+        this.tasks = pet.tasks;
     }
 
-    static getPets(user_id){
-        const petUrl = `http://localhost:3000/pets/`
+    static getPets(user_id) {
+        const petUrl = `http://localhost:3000/pets/`;
         return fetch(petUrl)
-        .then(resp => resp.json())
-        .then(json => Pet.renderPets(json, user_id))
+            .then((resp) => resp.json())
+            .then((json) => Pet.renderPets(json, user_id));
     }
 
-    static renderPets(json, u_id){
-        
-        let pets = json
-        let userPets = pets.filter(function(pet){
-            return pet.user_id === u_id})
-        userPets.map(pet => {
-            const div = document.getElementById('pets-container')
-            const p = document.createElement('div')
-            p.className = "card"
-            const name = document.createElement('h3')
-            name.innerText = `Name: ${pet.name}`
-            p.appendChild(name)
-            const kind = document.createElement('h3')
-            kind.innerText = `Kind: ${pet.kind}`
-            p.appendChild(kind)
-            const button = document.createElement('button')
-            button.innerText = 'My Tasks'
-            button.classList = 'task-btn'
-            button.setAttribute("id", pet.id)
-            p.appendChild(button)
-            div.appendChild(p)
-            
-            }
-        )
+    static renderPets(json, u_id) {
+        let pets = json;
+        let userPets = pets.filter(function(pet) {
+            return pet.user_id === u_id;
+        });
+        userPets.map((pet) => {
+            const div = document.getElementById("pets-container");
+            const p = document.createElement("div");
+            p.className = "card";
+            const name = document.createElement("h3");
+            name.innerText = `Name: ${pet.name}`;
+            p.appendChild(name);
+            const kind = document.createElement("h3");
+            kind.innerText = `Kind: ${pet.kind}`;
+            p.appendChild(kind);
+            const button = document.createElement("button");
+            button.innerText = "My Tasks";
+            button.classList = "task-btn";
+            button.setAttribute("id", pet.id);
+            p.appendChild(button);
+            div.appendChild(p);
+        });
     }
-    static newPetForm(user_id){
-        const petsContainer = document.getElementById('pets-container')
+    static newPetForm(user_id) {
+        const petsContainer = document.getElementById("pets-container");
 
         const newPetForm = `
         
@@ -49,128 +47,113 @@ class Pet {
         <input type = "text" id = "name" placeholder="Input your pet's name..." required ><br>
         <input type = "text" id = "kind" placeholder = "Input your pet type(cat, dog, fish, etc.) required"> <br>
         <input type = "submit"/> </form>
-        `
+        `;
 
-        petsContainer.insertAdjacentHTML('beforeend', newPetForm)
-        Pet.createPet(user_id)
+        petsContainer.insertAdjacentHTML("beforeend", newPetForm);
+        Pet.createPet(user_id);
     }
 
-    static createPet(user_id){
-        const petForm = document.getElementById('new-pet-form')
-        petForm.addEventListener('submit', function(e){
-            e.preventDefault()
-            fetch('http://localhost:3000/pets', {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    "Accept": "application/json"
-                },
-                body: JSON.stringify(
-                    {
+    static createPet(user_id) {
+        const petForm = document.getElementById("new-pet-form");
+        petForm.addEventListener("submit", function(e) {
+            e.preventDefault();
+            fetch("http://localhost:3000/pets", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                        Accept: "application/json",
+                    },
+                    body: JSON.stringify({
                         pet: {
                             name: e.target.children[1].value,
                             kind: e.target.children[3].value,
                             user_id: user_id,
-                            tasks: []
-                        }
-                    })
+                            tasks: [],
+                        },
+                    }),
                 })
-                .then(resp => {
-                    return resp.json()
+                .then((resp) => {
+                    return resp.json();
                 })
-                .then (pet => {
-                    const newPet = new Pet(pet)
-                    newPet.user_id = user_id
-                    newPet.displayPet()
-                    
+                .then((pet) => {
+                    const newPet = new Pet(pet);
+                    newPet.user_id = user_id;
+                    newPet.displayPet();
+                });
+        });
+    }
 
-                })
-            })
-        }
-        
+    displayPet() {
+        const div = document.getElementById("pets-container");
+        const p = document.createElement("div");
+        p.className = "card";
+        p.setAttribute("id", this.id);
+        const name = document.createElement("h3");
+        name.innerText = `Name: ${this.name}`;
+        p.appendChild(name);
+        const kind = document.createElement("h3");
+        kind.innerText = `Kind: ${this.kind}`;
+        p.appendChild(kind);
 
-    displayPet(){
-        const div = document.getElementById('pets-container')
-        const p = document.createElement('div')
-        p.className = "card"
-        p.setAttribute("id", this.id)
-        const name = document.createElement('h3')
-        name.innerText = `Name: ${this.name}`
-        p.appendChild(name)
-        const kind = document.createElement('h3')
-        kind.innerText = `Kind: ${this.kind}`
-        p.appendChild(kind)
+        const deleteButton = document.createElement("button");
+        deleteButton.innerHTML = "Delete Pet";
+        deleteButton.classList = "delete-btn";
+        deleteButton.setAttribute("id", this.id);
+        p.insertAdjacentElement("beforeend", deleteButton);
 
-        const deleteButton = document.createElement('button')
-        deleteButton.innerHTML = 'Delete Pet'
-        deleteButton.classList = 'delete-btn'
-        deleteButton.setAttribute("id", this.id)
-        p.insertAdjacentElement('beforeend', deleteButton)
+        deleteButton.addEventListener("click", (e) => {
+            e.preventDefault();
+            this.deletePet(e, this.id);
+            e.target.parentElement.remove();
+        });
 
-        deleteButton.addEventListener('click', (e) => {
-            e.preventDefault()
-            this.deletePet(e, this.id)
-            e.target.parentElement.remove()
-        })
+        const taskContainer = document.createElement("div");
+        taskContainer.setAttribute("id", `${this.id} - tasks-container`);
 
-        const taskContainer = document.createElement('div')
-        taskContainer.setAttribute('id', `${this.id} - tasks-container`)
-        
+        const button = document.createElement("button");
+        button.innerText = "My Tasks";
+        button.classList = "task-btn";
+        button.setAttribute("id", `btn-${this.id}`);
+        taskContainer.appendChild(button);
+        let tasks = this.tasks;
 
-        const button = document.createElement('button')
-        button.innerText = 'My Tasks'
-        button.classList = 'task-btn'
-        button.setAttribute("id", `btn-${this.id}`)
-        taskContainer.appendChild(button)
-        let tasks = this.tasks
+        button.addEventListener("click", (e) => {
+            if (!document.getElementById(`new-task-form-${this.id}`)) {
+                Task.newTaskForm(e, this.id);
+                let allTasks = document.createElement("div");
+                allTasks.setAttribute("id", `${this.id} - tasks`);
+                taskContainer.appendChild(allTasks);
+                const sortButton = document.createElement("button");
+                sortButton.innerText = "Sort by Alphabetical Order";
+                sortButton.classList = "sort-btn";
+                allTasks.appendChild(sortButton);
+                sortButton.addEventListener("click", (e) => {
+                    e.preventDefault();
+                    tasks.sort(function(a, b) {
+                        return a.title.localeCompare(b.title);
+                    });
 
-        
-        
-        button.addEventListener('click', (e)=>{
-            if (!document.getElementById(`new-task-form-${this.id}`)){
-                Task.newTaskForm(e, this.id)
-                let allTasks = document.createElement('div')
-                allTasks.setAttribute('id', `${this.id} - tasks`)
-                taskContainer.appendChild(allTasks)
-                const sortButton = document.createElement('button')
-                sortButton.innerText = 'Sort by Alphabetical Order'
-                sortButton.classList = 'sort-btn'
-                allTasks.appendChild(sortButton) 
-                sortButton.addEventListener('click', (e) => {
-                    e.preventDefault()
-                    tasks.sort(function(a,b) {
-                        return a.title.localeCompare(b.title)
-                    })
-
-                    let clearedTasks = document.getElementById(`${this.id} - tasks`)
-                    clearedTasks.innerHTML = ''
-                    tasks.forEach(function(task){
-                        let newTask = new Task(task)
-                        newTask.displayTask()
-                    })
-                    })
-                if (tasks){
-                    tasks.forEach(function(task){
-                        let newTask = new Task(task)
-                        newTask.displayTask()
-                    })
-                } 
-                    
-                
+                    let clearedTasks = document.getElementById(`${this.id} - tasks`);
+                    clearedTasks.innerHTML = "";
+                    tasks.forEach(function(task) {
+                        let newTask = new Task(task);
+                        newTask.displayTask();
+                    });
+                });
+                if (tasks) {
+                    tasks.forEach(function(task) {
+                        let newTask = new Task(task);
+                        newTask.displayTask();
+                    });
+                }
             }
-                
-             
-        }
-            
-        )
-        p.appendChild(taskContainer)
-        div.appendChild(p)
-   
+        });
+        p.appendChild(taskContainer);
+        div.appendChild(p);
     }
-    deletePet(e, pet_id){
-        fetch(`http://localhost:3000/pets/`+ pet_id, {
-        method: "DELETE"
-        })
+    deletePet(e, pet_id) {
+        fetch(`http://localhost:3000/pets/` + pet_id, {
+            method: "DELETE",
+        });
     }
-
 }
--- js_project_frontend/src/user.js
+++ js_project_frontend/src/user.js
@@ -1,74 +1,67 @@
-document.addEventListener('DOMContentLoaded', function(){
-    User.createUser()
-})
+document.addEventListener("DOMContentLoaded", function() {
+    User.createUser();
+});
 
 class User {
-    constructor(user){
-        this.id = user.id
-        this.name = user.name
-        this.pets = user.pets
-        this.tasks = user.tasks
+    constructor(user) {
+        this.id = user.id;
+        this.name = user.name;
+        this.pets = user.pets;
+        this.tasks = user.tasks;
     }
 
-    static createUser(){
-        const userForm = document.getElementById('new-user-form')
-        userForm.addEventListener('submit', function(e){
-            e.preventDefault()
-            fetch('http://localhost:3000/users', {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    "Accept": "application/json"
-                },
-                body: JSON.stringify(
-                    {
+    static createUser() {
+        const userForm = document.getElementById("new-user-form");
+        userForm.addEventListener("submit", function(e) {
+            e.preventDefault();
+            fetch("http://localhost:3000/users", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                        Accept: "application/json",
+                    },
+                    body: JSON.stringify({
                         user: {
-                            name: e.target.children[0].value
-                        }
-                    })
+                            name: e.target.children[0].value,
+                        },
+                    }),
                 })
-                .then(resp => {
-                    return resp.json()
+                .then((resp) => {
+                    return resp.json();
                 })
-                .then (user => {
-                    const newUser = new User(user)
-                    newUser.displayUser()
-                })
-            })
-        }
-
-        displayUser() {
-            const body = document.getElementById('index-container')
-            body.innerHTML = `
-            <h2>Welcome back, ${this.name}.</h2>
-            <h3>Manage your pets and record your tasks below.</h3>`
-
-            const petContainer = document.createElement('div')
-            petContainer.setAttribute('id', 'pets-container')
-            body.append(petContainer)
-            const taskContainer = document.createElement('div')
-            taskContainer.setAttribute('id', 'tasks-container')
-            
-            petContainer.append(taskContainer)
-            Pet.newPetForm(this.id)
-            const tasks = this.tasks
-            if (this.pets){
-                this.pets.forEach(function(pet){
-                    let newPet = new Pet(pet)
-                    newPet.tasks = []
-                    tasks.map((task)=>{
-                        if(task.pet_id === newPet.id){
-                            newPet.tasks.push(task)
-                        }
-                    })
-                    newPet.displayPet()
-                })
-            }
-            
-            
-            
-        }
-
-
-        
+                .then((user) => {
+                    const newUser = new User(user);
+                    newUser.displayUser();
+                });
+        });
     }
+
+    displayUser() {
+        const body = document.getElementById("index-container");
+        body.innerHTML = `
+            <h2>Welcome back, ${this.name}.</h2>
+            <h3>Manage your pets and record your tasks below.</h3>`;
+
+        const petContainer = document.createElement("div");
+        petContainer.setAttribute("id", "pets-container");
+        body.append(petContainer);
+        const taskContainer = document.createElement("div");
+        taskContainer.setAttribute("id", "tasks-container");
+
+        petContainer.append(taskContainer);
+        Pet.newPetForm(this.id);
+        const tasks = this.tasks;
+        if (this.pets) {
+            this.pets.forEach(function(pet) {
+                let newPet = new Pet(pet);
+                newPet.tasks = [];
+                tasks.map((task) => {
+                    if (task.pet_id === newPet.id) {
+                        newPet.tasks.push(task);
+                    }
+                });
+                newPet.displayPet();
+            });
+        }
+    }
+}
